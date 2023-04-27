1af1a78439f63eab18389db93baab8271dda1b6a
Eliemmos37 <eliemmos37@gmail.com>
2023-04-27 19:11:34 -0400

    new code added for back end

---
+++ mc_back.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+
+# view posts
+@app.route('/api/posts', methods=["GET"])
+def posts():
+    con = sqlite3.connect("minecraft.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor() 
+
+
+    try:
+        cur.execute("SELECT post_id, post, post_date FROM posts")
+    except sqlite3.Error as err:
+        con.commit()
+        con.close
+        print('Database error detected: ', err)
+        return jsonify({"error": "Database error"}), 500
+    response=[]
+    for row in cur:
+        response.append({
+            "post_id": row['post_id'],
+            "post": row['post'],
+            "post_date": row['post_date']
+        })
+
+
+    con.commit()
+    con.close
+    return jsonify(response), 200
+
+# create post
