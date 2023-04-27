4aec903442070350957dcc344c28731d2de156cc
Legoolego <olegskorinskiy@gmail.com>
2023-04-27 17:07:43 -0600

    start of development

---
+++ minecraft.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import json
+import sqlite3
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+
+@app.route('/minecraft/posts', methods=["GET"])
+def show_posts():
+    con = sqlite3.connect("minecraft.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    try:
+        cur.execute("SELECT post_id, post, post_date FROM posts")
+    except sqlite3.Error as err:
+        con.commit()
+        con.close
+        print('Database error detected: ', err)
+        return jsonify({"error": "Database error"}), 500
+        
+    response = []
+    for row in cur:
+        response.append({
+            "post_id": row['post_id'],
+            "post": row['post'],
+            "post_date": row['post_date'],
+        })
+    con.commit()
+    con.close
+    return jsonify(response), 200
