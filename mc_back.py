e9df0049e58a6066f51e49631c3dff3169bdd101
Eliemmos37 <eliemmos37@gmail.com>
2023-06-01 19:07:17 -0400

    new backend code

--- mc_back.py
+++ mc_back.py
@@ -44,5 +44,90 @@ def create_post():
     except sqlite3.Error as err:
         con.commit()
         con.close
+        return jsonify({"error": "Database error"}), 500
+    con.commit()
+    con.close
+    return jsonify({"status": "OK", "created_id": cur.lastrowid}), 201
+
+#Delete post
+@app.route('/api/post/<int:id>', methods=["DELETE"])
+def remove_post(id):
+    con = sqlite3.connect("minecraft.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    try:
+        cur.execute("DELETE FROM posts WHERE post_id=?", [id])
+    except sqlite3.Error as err:
+        con.commit()
+        con.close
         print('Database error detected: ', err)
         return jsonify({"error": "Database error"}), 500
+
+    # Return Successful Response
+    con.commit()
+    con.close
+    return "OK", 200
+
+# view tips
+@app.route('/api/tips', methods=["GET"])
+def tips():
+    con = sqlite3.connect("minecraft1.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor() 
+
+
+    try:
+        cur.execute("SELECT tip_id, tip, description FROM survival_tips")
+    except sqlite3.Error as err:
+        con.commit()
+        con.close
+        print('Database error detected: ', err)
+        return jsonify({"error": "Database error"}), 500
+    response=[]
+    for row in cur:
+        response.append({
+            "tip_id": row['tip_id'],
+            "tip": row['tip'],
+            "description": row['description']
+        })
+
+
+    con.commit()
+    con.close
+    return jsonify(response), 200
+
+#Delete tip
+@app.route('/api/tips/<int:id>', methods=["DELETE"])
+def remove_tip(id):
+    con = sqlite3.connect("minecraft1.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    try:
+        cur.execute("DELETE FROM survival_tips WHERE tip_id=?", [id])
+    except sqlite3.Error as err:
+        con.commit()
+        con.close
+        print('Database error detected: ', err)
+        return jsonify({"error": "Database error"}), 500
+
+    # Return Successful Response
+    con.commit()
+    con.close
+    return "OK", 200
+
+# create tip
+@app.route('/api/tips', methods=["POST"])
+def create_tip():
+    con = sqlite3.connect("minecraft1.db")
+    cur = con.cursor()
+    tip = request.json['tip']
+    description = request.json['description']
+    try:
+        cur.execute("INSERT INTO survival_tips (tip, description) VALUES (?)", (tip, description))
+    except sqlite3.Error as err:
+        con.commit()
+        con.close
+        return jsonify({"error": "Database error"}), 500
+    con.commit()
+    con.close
+    return jsonify({"status": "OK", "created_id": cur.lastrowid}), 201
